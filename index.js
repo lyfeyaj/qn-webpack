76ff2ef99eb8290f4a858970412cc9ba357a96c9
lyfeyaj <lyfeyaj@gmail.com>
2017-03-04 18:52:45 +0800

    add hash with length support and exclude option

--- index.js
+++ index.js
@@ -4,9 +4,36 @@ const qiniu = require('qiniu');
 const path = require('path');
 const ora = require('ora');
 
+// Contants
+const REGEXP_HASH = /\[hash(?::(\d+))?\]/gi;
+
+// Uploading progress tip
 const tip = (uploaded, total) => {
   let percentage = Math.round(uploaded / total * 100);
   return `Uploading to Qiniu CDN: ${percentage}% ${uploaded}/${total} files uploaded`;
+};
+
+// Replace path variable by hash with length
+const withHashLength = (replacer) => {
+  return function(_, hashLength) {
+    const length = hashLength && parseInt(hashLength, 10);
+    const hash = replacer.apply(this, arguments);
+    return length ? hash.slice(0, length) : hash;
+  };
+};
+
+// Perform hash replacement
+const getReplacer = (value, allowEmpty) => {
+  return function(match) {
+    // last argument in replacer is the entire input string
+    const input = arguments[arguments.length - 1];
+    if(value === null || value === undefined) {
+      if(!allowEmpty) throw new Error(`Path variable ${match} not implemented in this context of qn-webpack plugin: ${input}`);
+      return '';
+    } else {
+      return `${value}`;
+    }
+  };
 };
 
 module.exports = class QiniuPlugin {
@@ -25,12 +52,39 @@ module.exports = class QiniuPlugin {
       let bucket = this.options.bucket;
       let uploadPath = this.options.path || '[hash]';
 
-      uploadPath = uploadPath.replace('[hash]', hash);
+      uploadPath = uploadPath.replace(REGEXP_HASH, withHashLength(getReplacer(hash)));
 
       let filesNames = Object.keys(assets);
-      let totalFiles = filesNames.length;
+      let totalFiles = 0;
       let uploadedFiles = 0;
       let promises = [];
+
+      // Mark finished
+      let _finish = (err) => {
+        spinner.succeed();
+        // eslint-disable-next-line no-console
+        console.log('\n');
+        callback(err);
+      };
+
+      // Filter files that should be uploaded to Qiniu CDN
+      filesNames = filesNames.filter(fileName => {
+        let file = assets[fileName] || {};
+
+        // Ignore unemitted files
+        if (!file.emitted) return false;
+
+        // Check excluced files
+        if (
+          this.options.exclude &&
+          this.options.exclude instanceof RegExp &&
+          this.options.exclude.test(fileName)
+        ) return false;
+
+        return true;
+      });
+
+      totalFiles = filesNames.length;
 
       // eslint-disable-next-line no-console
       console.log('\n');
@@ -39,16 +93,8 @@ module.exports = class QiniuPlugin {
         color: 'green'
       }).start();
 
-      let _finish = (err) => {
-        spinner.succeed();
-        // eslint-disable-next-line no-console
-        console.log('\n');
-        callback(err);
-      };
-
       filesNames.map(fileName => {
         let file = assets[fileName] || {};
-        if (!file.emitted) return;
 
         let key = path.posix.join(uploadPath, fileName);
         let token = new qiniu.rs.PutPolicy(`${bucket}:${key}`).token();
