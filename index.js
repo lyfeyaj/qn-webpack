377514fee6fbce3179f5180dac30e152752b7a35
lyfeyaj <lyfeyaj@gmail.com>
2018-07-26 18:33:57 +0800

    add webpack@4 support

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ module.exports = class QiniuPlugin {
   }
 
   apply(compiler) {
-    compiler.plugin('after-emit', (compilation, callback) => {
+    compiler.hooks.afterEmit.tapAsync((compilation, callback) => {
 
       let assets = compilation.assets;
       let hash = compilation.hash;
