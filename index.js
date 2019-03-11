656c7a33868df044fbc4e8bd582ab94b9ce0695b
lyfeyaj <lyfeyaj@gmail.com>
2019-03-11 11:56:53 +0800

    add retry support

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ const isString = require('lodash.isstring');
 const REGEXP_HASH = /\[hash(?::(\d+))?\]/gi;
 
 // Uploading progress tip
-const tip = (uploaded, total) => {
+const tip = (uploaded, failed, total, retrying) => {
   let percentage = Math.round(uploaded / total * 100);
-  return `Uploading to Qiniu CDN: ${percentage}% ${uploaded}/${total} files uploaded`;
+  let msg = retrying ? 'Retrying failed files:' : 'Uploading to Qiniu CDN:';
+  return `${msg} ${percentage}% ${uploaded}/${total} files uploaded, ${failed} files failed`;
 };
 
 // Replace path variable by hash with length
@@ -53,6 +54,7 @@ module.exports = class QiniuPlugin {
       let exclude = isRegExp(this.options.exclude) && this.options.exclude;
       let include = isRegExp(this.options.include) && this.options.include;
       let batch = this.options.batch || 20;
+      let maxRetryTimes = this.options.maxRetryTimes || 3;
       let mac = new qiniu.auth.digest.Mac(this.options.accessKey, this.options.secretKey);
       let qiniuConfig = new qiniu.conf.Config();
       let bucket = this.options.bucket;
@@ -64,6 +66,8 @@ module.exports = class QiniuPlugin {
       let filesNames = Object.keys(assets);
       let totalFiles = 0;
       let uploadedFiles = 0;
+      let retryFiles = [];
+      let retryFilesCountDown = 0;
 
       // Mark finished
       let _finish = (err) => {
@@ -94,12 +98,12 @@ module.exports = class QiniuPlugin {
       // eslint-disable-next-line no-console
       console.log('\n');
       let spinner = ora({
-        text: tip(0, totalFiles),
+        text: tip(0, retryFiles.length, totalFiles, false),
         color: 'green'
       }).start();
 
       // Perform upload to qiniu
-      const performUpload =function(fileName) {
+      const performUpload = function(fileName, retrying) {
         let file = assets[fileName] || {};
         let key = path.posix.join(uploadPath, fileName);
         let putPolicy = new qiniu.rs.PutPolicy({ scope: bucket + ':' + key });
@@ -107,17 +111,60 @@ module.exports = class QiniuPlugin {
         let formUploader = new qiniu.form_up.FormUploader(qiniuConfig);
         let putExtra = new qiniu.form_up.PutExtra();
 
-        return new Promise((resolve, reject) => {
+        return new Promise((resolve) => {
           let begin = Date.now();
-          formUploader.putFile(uploadToken, key, file.existsAt, putExtra, function(err, body) {
-            uploadedFiles++;
-            spinner.text = tip(uploadedFiles, totalFiles);
 
-            if (err) return reject(err);
-            body.duration = Date.now() - begin;
-            resolve(body);
-          });
+          formUploader.putFile(
+            uploadToken,
+            key,
+            file.existsAt,
+            putExtra,
+            function(err, body) {
+              // handle upload error
+              if (err) {
+                // eslint-disable-next-line no-console
+                console.log(`Upload file ${fileName} failed: ${err.message || err.name || err.stack}`);
+                if (!~retryFiles.indexOf(fileName)) retryFiles.push(fileName);
+              } else {
+                uploadedFiles++;
+              }
+
+              spinner.text = tip(uploadedFiles, retryFiles.length, totalFiles, retrying);
+              body.duration = Date.now() - begin;
+              resolve(body);
+            });
         });
+      };
+
+      // Retry all failed files one by one
+      const retryFailedFiles = function(err) {
+        if (err) {
+          // eslint-disable-next-line no-console
+          console.log('\n');
+          return Promise.reject(err);
+        }
+
+        if (retryFilesCountDown < 0) retryFilesCountDown = 0;
+
+        // Get batch files
+        let _files = retryFiles.splice(
+          0,
+          batch <= retryFilesCountDown ? batch : retryFilesCountDown
+        );
+        retryFilesCountDown = retryFilesCountDown - _files.length;
+
+
+        if (_files.length) {
+          return Promise.all(
+            _files.map(file => performUpload(file, true))
+          ).then(() => retryFailedFiles(), retryFailedFiles);
+        } else {
+          if (retryFiles.length) {
+            return Promise.reject(new Error('File uploaded failed'));
+          } else {
+            return Promise.resolve();
+          }
+        }
       };
 
       // Execute stack according to `batch` option
@@ -133,14 +180,17 @@ module.exports = class QiniuPlugin {
 
         if (_files.length) {
           return Promise.all(
-            _files.map(performUpload)
+            _files.map(file => performUpload(file, false))
           ).then(() => execStack(), execStack);
         } else {
           return Promise.resolve();
         }
       };
 
-      execStack().then(() => _finish(), _finish);
+      execStack().then(() => {
+        retryFilesCountDown = retryFiles.length * maxRetryTimes;
+        return retryFailedFiles();
+      }).then(() => _finish(), _finish);
     };
 
     // For webpack >= 4
